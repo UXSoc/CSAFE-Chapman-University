c97cb2da283bf0c46b5de2f0c7a333c028afa96d
Xavi Ablaza <xlablaza@gmail.com>
2018-10-21 01:25:07 -0700

    WIP
    Adding dot to show user location

--- app/javascript/components/RiderMapv2.js
+++ app/javascript/components/RiderMapv2.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, {Marker} from "react-map-gl";
 
 class RiderMapv2 extends React.Component {
   state = {
@@ -9,17 +9,43 @@ class RiderMapv2 extends React.Component {
       height: "66vh",
       latitude: 37.7577,
       longitude: -122.4376,
-      zoom: 8
+      zoom: 14
     }
   };
 
+  options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+  _locateError = () => {
+    console.warn('Could not locate the user');
+  }
+
+  _locateUser = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const viewport = {...this.state.viewport, longitude: position.coords.longitude, latitude: position.coords.latitude};
+      this.setState({viewport});
+    },
+    this._locateError,
+    this.options
+    );
+  }
+
   render() {
     return (
-      <ReactMapGL
-      {...this.state.viewport}
-      onViewportChange={(viewport) => this.setState({viewport})}
-      mapboxApiAccessToken={this.props.token}
-      />
+      <div>
+        <ReactMapGL
+        {...this.state.viewport}
+        onViewportChange={(viewport) => this.setState({viewport})}
+        mapboxApiAccessToken={this.props.token}
+        />
+        <Marker latitude={33.8242184} longitude={-117.838183} offsetLeft={-20} offsetTop={-10}>
+          <div>You are here</div>
+        </Marker>
+        <button onClick={this._locateUser}>Locate User</button>
+      </div>
     );
   }
 }
